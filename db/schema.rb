120b3437357e050be3e78e4d6add1aecbbc8724e
ramusus <ramusus@gmail.com>
2012-05-25 16:08:35 +0400

    updated server

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120511164359) do
+ActiveRecord::Schema.define(:version => 20120511170342) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
